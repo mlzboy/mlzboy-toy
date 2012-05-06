a35124de1169d1b6eedb1bd8dcd9a17d97122a5d
frederick.mao(毛凌志) <frederick.mao@gmail.com>
2012-05-07 05:23:40 +0800

    second commit add email minder feature for blog rank

--- rank.py
+++ rank.py
@@ -4,6 +4,7 @@ import datetime
 from google.appengine.ext import db
 from google.appengine.api import users
 from google.appengine.ext.webapp import Request
+from google.appengine.api import mail
 import os
 
 class Rank(db.Model):
@@ -31,4 +32,23 @@ print q
 
 for r in Rank.all().order("-date"):
     print r.rank,r.date,r.ip
+    
+#ranks=Rank.gql("order by date desc LIMIT 2")
+r=Rank.all().order("-date").fetch(2)
+print len(r)
+if len(r)==2:
+  if r[0].rank<>r[1].rank:
+    print "send email"
+    mail.send_mail(sender="frederick.mao@gmail.com",
+                  to="maolingzhi@gmail.com",
+                  subject="[GAE]博客园排名升至"+str(r[0].rank)+"位",
+                  body="""
+    Dear mao:
+    
+    your blog rank from %s to %s,good job!
+    """%(r[0].rank,r[1].rank))
+  else:
+    print "the same 22"
+else:
+  print "the same"
         
